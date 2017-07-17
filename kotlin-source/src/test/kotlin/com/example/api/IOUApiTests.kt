61cde63a40e64e2433b40c0b8b9f1199b0272c8a
Prathap Reddy <PrathapaReddy.CC@cognizant.com>
2017-07-17 23:12:14 +0530

    Latest m13 (#1)
    * corda tutorial release-m13
    
    * consume state code
    
    * updated with receive flow
    
    * Update the README with API details.
    
    * circle.yml added
    
    * updated readme - CI status
    
    * added parallelism
    
    * API tests added

---
+++ kotlin-source/src/test/kotlin/com/example/api/IOUApiTests.kt
@@ -0,0 +1,97 @@
+package com.example.api
+
+import com.google.common.util.concurrent.Futures
+import net.corda.core.getOrThrow
+import net.corda.core.node.services.ServiceInfo
+import net.corda.core.utilities.ALICE
+import net.corda.core.utilities.BOB
+import net.corda.core.utilities.DUMMY_NOTARY
+import net.corda.node.services.transactions.SimpleNotaryService
+import net.corda.nodeapi.User
+import net.corda.testing.IntegrationTestCategory
+import net.corda.testing.driver.driver
+import net.corda.testing.http.HttpApi
+import org.bouncycastle.asn1.x500.X500Name
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Test
+import org.json.simple.JSONObject
+
+class IOUApiTests : IntegrationTestCategory {
+
+    @Test
+    fun `run TradeTxApi`() {
+        driver(isDebug = true) {
+            val rpcUser = User("user", "password", permissions = setOf())
+
+            val (controller, nodeA, nodeB) = Futures.allAsList(
+                    startNode(DUMMY_NOTARY.name, setOf(ServiceInfo(SimpleNotaryService.type))),
+                    startNode(ALICE.name, rpcUsers = listOf(rpcUser)),
+                    startNode(BOB.name, rpcUsers = listOf(rpcUser))).getOrThrow()
+
+            println("All nodes started")
+
+            val (controllerAddr, nodeAAddr, nodeBAddr) = Futures.allAsList(
+                    startWebserver(controller),
+                    startWebserver(nodeA),
+                    startWebserver(nodeB)).getOrThrow().map { it.listenAddress }
+
+            println("All webservers started")
+
+            val (_, nodeAApi, nodeBApi) = listOf(controller, nodeA, nodeB)
+                    .zip(listOf(controllerAddr, nodeAAddr, nodeBAddr)).map {
+                val mapper = net.corda.jackson.JacksonSupport.createDefaultMapper(it.first.rpc)
+
+                HttpApi.fromHostAndPort(it.second, "api/example", mapper = mapper)
+            }
+
+            Assert.assertThat(getMe(nodeAApi), CoreMatchers.containsString(nodeA.nodeInfo.legalIdentity.name.toString()))
+            Assert.assertThat(getPeers(nodeAApi), CoreMatchers.allOf(
+                    CoreMatchers.containsString(nodeB.nodeInfo.legalIdentity.name.toString())
+            ))
+
+            Assert.assertThat(getMe(nodeBApi), CoreMatchers.containsString(nodeB.nodeInfo.legalIdentity.name.toString()))
+            Assert.assertThat(getPeers(nodeBApi), CoreMatchers.allOf(
+                    CoreMatchers.containsString(nodeA.nodeInfo.legalIdentity.name.toString())
+            ))
+
+            Assert.assertEquals(getTransactions(nodeAApi).size, 0)
+            Assert.assertEquals(getTransactions(nodeBApi).size, 0)
+//            Assert.assertTrue(proposeTransaction(nodeAApi, nodeB.nodeInfo.legalIdentity.name))
+//            Assert.assertEquals(getTransactions(nodeAApi).size, 1)
+//            Assert.assertEquals(getTransactions(nodeBApi).size, 1)
+        }
+    }
+
+    private fun getMe(nodeApi: HttpApi): String {
+        println("Getting Me from ${nodeApi.root}")
+        val me = nodeApi.getJson<Any>("me")
+        return me.toString()
+    }
+
+    private fun getPeers(nodeApi: HttpApi): String {
+        println("Getting peers from ${nodeApi.root}")
+        val peers = nodeApi.getJson<Any>("peers")
+        return peers.toString()
+    }
+
+    private fun getTransactions(nodeApi: HttpApi): Array<*> {
+        println("Getting transactions from ${nodeApi.root}")
+        val transactions = nodeApi.getJson<Array<*>>("ious")
+        return transactions
+    }
+
+//    private fun proposeTransaction(nodeApi: HttpApi,
+//                                   buyerName: X500Name): Boolean {
+//        println("Proposing transaction from ${nodeApi.root} to $buyerName")
+//        val jsonData = JSONObject(
+//                mapOf("contractId" to 10,
+//                        "deliveryDate" to "2017-10-31T18:00:00.000Z",
+//                        "amount" to 100,
+//                        "purchaseOrderNumber" to 123456,
+//                        "sellerId" to 1234,
+//                        "buyerId" to 4567))
+//        val transaction = nodeApi.putJson("propose?partyName=$buyerName", jsonData)
+//        return transaction
+//    }
+}
